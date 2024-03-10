8b734f02ac2465792e6ce10ddfd67d352b2b5306
Rahul Bhardwaj <140134424+Rahul-Bhardwaj0405@users.noreply.github.com>
2024-03-10 21:55:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+// document.querySelector("h1").innerHTML="Good Bye!!";  //innerHTML means text inside "h1".
+
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela";
+
